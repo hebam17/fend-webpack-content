9bd5a2240d211070feb557bad9ea150ac4300644
unknown <hebamarzouk17@gmail.com>
2023-06-17 20:56:19 +0300

    Add the json loader to the config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
 const path = require("path");
 const webpack = require("webpack");
+
 module.exports = {
   entry: "./src/client/index.js",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.json$/,
+        loader: "json-loader",
+      },
+    ],
+  },
 };
